71d4941c3994315a64e36387d5f0478adf511ae7
Ramon Dias <ramondiaz.ads@gmail.com>
2024-01-10 13:07:38 -0300

    generate component atributos

---
+++ src/app/comp-atributos/comp-atributos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comp-atributos',
+  standalone: true,
+  imports: [],
+  templateUrl: './comp-atributos.component.html',
+  styleUrl: './comp-atributos.component.css'
+})
+export class CompAtributosComponent {
+
+}
